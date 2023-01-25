804303f2c2c656053c0b4755f9a161caae7f098b
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2023-01-25 15:53:49 +0600

    Switching content implemented

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,37 +1,86 @@
 import React, { useState } from 'react';
 import { AiFillHome } from 'react-icons/ai';
-import { BiUpArrow } from 'react-icons/bi';
+import { BiUpArrow, BiUserCircle } from 'react-icons/bi';
 
 const Home = () => {
     const [user, setUser] = useState(false)
     const [view, setView] = useState(false)
     const [home, setHome] = useState(false)
+    const [userManu, setUserManu] = useState(false)
+
+
+    const handleView = () => {
+        console.log("from handle");
+        if (user == true) {
+            setUser(false)
+            setTimeout(() => {
+                setView(!view)
+            }, 500)
+
+        }
+        else {
+            setView(!view)
+        }
+    }
+    const handleUser = () => {
+        console.log("from handle");
+        if (view == true) {
+            setView(false)
+            setTimeout(() => {
+                setUser(!user)
+            }, 500)
+
+        }
+        else {
+            setUser(!user)
+        }
+    }
+
+
     return (
-        <div className='mt-5'>
-            <div className='bg-slate-900 grid grid-cols-10'>
-                <div className='col-span-2 mt-8'>
-                    <div className=' p-2   h-[100vh]'>
-                        <div className='hover:bg-slate-600 hover:rounded-l-2xl flex justify-center items-center'>
-                            <h1 className='text-white mr-3 ml-3 bg-red-500 p-2 rounded-full'><AiFillHome /></h1>
-                            <button onMouseEnter={() => setHome(true)} onMouseLeave={() => setHome(false)} className={`hover:${() => setHome(!home)} text-white px-2`}>Home</button>
+        <div className=''>
+            <div className='bg-slate-900 flex'>
+
+
+                <div onMouseEnter={() => setHome(true)} onMouseLeave={() => setHome(false)} className="bg-gray-400 w-[60px] hover:w-[200px] transition-all duration-500 ease-in-out">
+                    <div className='ml-2 mt-2 h-[100vh]'>
+                        <div className={home == true ? "bg-slate-600 rounded-l-2xl flex justify-around items-center" : "flex justify-between items-center"}>
+                            <h1 className='text-white bg-red-500 p-2 rounded-full'><AiFillHome /></h1>
+                            {/* <button className={home == true ? "block text-white transition-all duration-500 ease-in-out" : "hidden"}></button> */}
+                            <h1 className={home == true ? "text-white text-xl block z-99" : "hidden w-3 "}>Home</h1>
                         </div>
                     </div>
                 </div>
-                <div className={home == true ? "col-span-8" : "hidden"}>
-                    <div className='ml-5 bg-slate-700 p-3 my-5 rounded-xl'>
-                        <div className=' my-5 flex flex-col'>
-                            <div className=' '>
-                                <button onClick={() => setUser(!user)} className={user == true ? "rounded-t-2xl bg-red-400 w-full text-white text-xl" : " text-white w-full  p-2 text-xl flex justify-center bg-red-400 rounded-2xl"}>User  <h1 className='flex justify-center items-center text-white'><BiUpArrow className={user == true ? "opacity-0" : "m-3 w-9/10 mr-auto"} /></h1></button>
-
-                            </div>
-                            <h1 className={user == true ? 'h-[200px]  bg-white text-black transition-all duration-500 ease-in-out' : "h-[0px] opacity-0"}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ea quam dignissimos veritatis est unde id modi aliquid similique minima, laboriosam deserunt nostrum possimus iure earum eveniet aut ratione! Doloremque?</h1>
+                <div className=" w-[80%] ">
+                    <div className='flex justify-end items-center mt-3'>
+                        <div className='bg-gray-400 flex justify-center items-center rounded-3xl  p-2 btn'>
+                            <h1 className='text-white text-2xl mr-3'><BiUserCircle /></h1>
+                            <h1 className='text-white text-2xl'>User</h1>
+                            <button className='flex'><BiUserCircle /> User</button>
                         </div>
-                        <div className=' my-5 flex flex-col'>
-                            <div className=' '>
-                                <button onClick={() => setView(!view)} className={view == true ? "rounded-t-2xl bg-red-400 w-full text-white text-xl" : " text-white w-full  p-2 text-xl flex justify-center bg-red-400 rounded-2xl"}>View <h1 className=' text-white'><BiUpArrow className={view == true ? "opacity-0" : "w-9/10 my-auto"} /></h1></button>
+                    </div>
+                    <div className='ml-5 bg-slate-700 p-3 my-5 rounded-xl flex flex-col'>
+                        <div className={user == true ? " my-5 flex flex-col order-last transition-all duration-500 ease-in-out" : "my-5 flex flex-col transition-all duration-500 ease-in-out"}>
+                            <div onClick={handleUser} className={user == true ? "bg-red-400 flex justify-between w-full h-[40px] rounded-t-2xl" : "bg-red-400 flex justify-between w-full rounded-2xl h-[40px]"}>
+
+                                <button className={user == true ? " w-[95%] text-white text-xl" : " text-white w-[95%]  p-2 text-xl flex justify-center "}>User  </button>
+                                <div className='w-[5%] flex justify-center items-center'>
+                                    <BiUpArrow className={user == true ? "rotate-180 transition-all duration-500 ease-in-out text-white" : "   transition-all duration-500 ease-in-out text-white"} />
+                                </div>
 
                             </div>
-                            <div className={view == true ? 'h-[200px]  bg-white text-black transition-all duration-500 ease-in-out rounded-b-xl' : "h-[0px] opacity-0"}>
+                            <h1 className={user == true ? 'h-[200px]  bg-white text-black transition-all duration-500 ease-in-out rounded-b-xl' : "h-[0px] opacity-0 transition-all duration-500 ease-in-out"}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ea quam dignissimos veritatis est unde id modi aliquid similique minima, laboriosam deserunt nostrum possimus iure earum eveniet aut ratione! Doloremque?</h1>
+                        </div>
+                        <div className={view == true ? " my-5 flex flex-col order-last transition-all duration-500 ease-in-out" : "my-5 flex flex-col transition-all duration-500 ease-in-out"}>
+                            <div onClick={handleView} className={view == true ? "bg-red-400 flex justify-between w-full rounded-t-2xl h-[40px]" : "bg-red-400 flex justify-between w-full h-[40px] rounded-2xl"}>
+
+                                <button className={view == true ? " w-[95%] text-white text-xl" : " text-white w-[95%]  p-2 text-xl flex justify-center "}>View  </button>
+                                <div className='w-[5%] flex justify-center items-center'>
+                                    <BiUpArrow className={view == true ? "rotate-180 transition-all duration-500 ease-in-out text-white" : "   transition-all duration-500 ease-in-out text-white"} />
+                                </div>
+
+                            </div>
+                            <div className={view == true ? 'h-[200px]  bg-white text-black transition-all duration-500 ease-in-out rounded-b-xl' : "h-[0px] opacity-0 transition-all duration-500 ease-in-out"}>
                                 <h1 >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ea quam dignissimos veritatis est unde id modi aliquid similique minima, laboriosam deserunt nostrum possimus iure earum eveniet aut ratione! Doloremque?</h1>
                             </div>
                         </div>
